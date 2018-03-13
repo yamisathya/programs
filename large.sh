963ce17b800d1bb690815c2a188180719d4b7e23
yamisathya <36188501+yamisathya@users.noreply.github.com>
2018-03-13 08:13:25 +0000

    Add files via upload

---
+++ large.sh
@@ -0,0 +1,15 @@
+echo "enter  num"
+read a
+read b
+read c
+if [ $a -gt $b -a $a -gt $c ]
+then
+echo "$a largest"
+elif  [ $b -gt $a -a $b -gt $c ]
+then
+echo "$b largest"
+else
+echo "$c largest"
+fi
+
+
